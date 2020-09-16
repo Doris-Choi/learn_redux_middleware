9cbc3287af3373d2034b68b4b319ab424a1abd94
doris <dychoi9298@gmail.com>
2020-09-16 15:23:41 +0900

    👩‍💻 사용자 경험 개선: 기존 Post reload 시 로딩중 표시 없도록 개선

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -2,19 +2,20 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Post from '../components/Post';
 import { getPost, clearPost } from '../modules/posts';
+import { reducerUtils } from '../lib/asyncUtills';
 
 function PostContainer({ postId }) {
-  const { data, loading, error } = useSelector((state) => state.posts.post);
+  const { data, loading, error } = useSelector(
+    (state) => state.posts.post[postId] || reducerUtils.initial(),
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if (data) return;
     dispatch(getPost(postId));
-    return () => {
-      dispatch(clearPost());
-    };
   }, [postId, dispatch]);
 
-  if (loading) return <div>로딩중...</div>;
+  if (loading && !data) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
