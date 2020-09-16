87ef571875138dd71221ddc296f26d12fe64022a
doris <dychoi9298@gmail.com>
2020-09-16 17:30:49 +0900

    🌐 json-server 사용하기

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,30 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-// { id, title, body }
-const posts = [
-  {
-    id: 1,
-    title: '리덕스 미들웨어를 배워봅시다',
-    body: '리덕스 미들웨어를 직접 만들어보면 이해가 쉽다.',
-  },
-  {
-    id: 2,
-    title: '리덕스 thunk를 배워봅시다',
-    body: '리덕스 thunk 직접 만들어보면 이해가 쉽다.',
-  },
-  {
-    id: 3,
-    title: '리덕스 saga를 배워봅시다',
-    body: '리덕스 saga를 직접 만들어보면 이해가 쉽다.',
-  },
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get('/posts');
+  return response.data;
 };
 
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const response = await axios.get(`/posts/${id}`);
+  return response.data;
 };
--- src/index_browserRouter.js
+++ src/index_browserRouter.js
@@ -7,30 +7,21 @@ import rootReducer from './modules';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-
-const customHistory = createBrowserHistory();
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(
-      ReduxThunk.withExtraArgument({ history: customHistory }),
-      logger,
-    ),
-  ),
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger)),
 );
 
 ReactDOM.render(
-  <Router history={customHistory}>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
